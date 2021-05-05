31ab11edb6c2278069c1494f9aabe9ec7baa52cd
Thiago Silvério <thiagocrokao@gmail.com>
2021-05-05 16:53:45 -0300

    Foi adionado movimento aos personagens e ao ambiente

--- JogosHTML5/jogo1/js/js.js
+++ JogosHTML5/jogo1/js/js.js
@@ -3,8 +3,33 @@ function start() { // Inicio da fun��o start()
 	$("#inicio").hide();
 	
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+
+	//Principais variбveis do jogo
+	
+
+	var jogo = {}
+	
+	//Game Loop
+	
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	
+	} // Fim da funзгo loop()
+
+
+	//Funзгo que movimenta o fundo do jogo
+
+	function movefundo() {
+
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		$("#fundoGame").css("background-position",esquerda-2);
+		
+		} // fim da funзгo movefundo()
 } // Fim da fun��o start
