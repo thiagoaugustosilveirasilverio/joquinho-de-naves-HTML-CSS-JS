159accfa87ed8b3e4440af1a7a68d47a7f70325d
Thiago Silvério <thiagocrokao@gmail.com>
2021-05-05 16:25:29 -0300

    Foi adionado vários arquivos já pré disponivéis pelo instrutor

---
+++ JogosHTML5/jogo1/js/js.js
@@ -0,0 +1,10 @@
+function start() { // Inicio da fun��o start()
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+} // Fim da fun��o start
